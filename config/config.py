662b49be556afa9f079acb853a3250a4f6c1fc8f
WizardMike1 <aryanmadhav776@gmail.com>
2024-10-14 15:46:42 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -46,7 +46,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/WizardMike1/extraplugin",
+    "https://github.com/qnxanjali/Extra-Plugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
